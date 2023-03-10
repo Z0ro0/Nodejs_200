01d17c36c2fe49ab560a467662114ceddd0223ac
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-10 23:21:36 +0900

    백틱으로 포맷팅(이 문법 더 권장)

---
+++ backtick.js
@@ -0,0 +1,12 @@
+const greeting1 = 'hello';
+const greeting2 = 'bye';
+const name1 = 'Lisa';
+const name2 = 'Rosy'
+
+const statement1 = `${greeting1}! my name is ${name1}`; //hello Lisa
+const statement2 = `${greeting2}! my name is ${name2}`; // bye Rosy
+
+console.log(`${greeting1}! my name is ${name1}`);
+console.log(`${greeting2}! my name is ${name2}`);
+console.log(statement1);
+console.log(statement2);
