6afc70a306a05e3425d984bb67d0c14c10bf5042
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-10 23:11:42 +0900

    포멧팅해서 출력

---
+++ formatting.js
@@ -0,0 +1,8 @@
+console.log('name:%s', 'kyeongrok');
+console.log('age:%d', 31);
+console.log('pi:%d', 3.14);
+console.log('math:%d science:%d', 92, 84);
+console.log('name:%s age:%d', 'kyeongrok', 31);
+
+//%s인지 %d인지 헷갈림 -> %s
+console.log('name:%s math:%s science:%s', 'kyeongrok', 92, 84);
