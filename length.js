cab1ff5e94cafcb541e0d984c33dcf4be34f4532
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-11 10:07:16 +0900

    length로 길이 구하고 첫 글자 마지막 글자 출력

---
+++ length.js
@@ -0,0 +1,10 @@
+const string = 'hello';
+const string2 = 'The method then selects elements from the start argument, and up to (but not including) the end argumenr.';
+
+//길이 출력
+console.log(string.length);
+console.log(string2.length);
+
+//첫글자 마지막글자 출력
+console.log(string[0]);
+console.log(string2[104]);
