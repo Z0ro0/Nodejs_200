ebcd65c2cb767b9851cd17828f4fe5ed1dd299fa
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-11 11:29:37 +0900

    for of 반복문

---
+++ forof.js
@@ -0,0 +1,9 @@
+const userList = [
+    {name: 'Lisa', age: 31, score: 90},
+    {name: 'Rosa', age: 28, score: 75},
+    {name: 'Nasa', age: 34, score: 87}
+];
+
+for(const user of userList){//userList 개수만큼 반복
+    console.log(user);
+}
