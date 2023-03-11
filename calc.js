f3ded19377eca5f44d8167b7e8c8987916a9d63c
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-11 09:56:44 +0900

    사칙연산

---
+++ calc.js
@@ -0,0 +1,12 @@
+const number10 = 10;
+const number20 = 20;
+
+const plus = number10 + number20;
+const minus = number10 - number20;
+const multi = number10 * number20;
+const div = number10 / number20;
+
+console.log(plus);
+console.log(minus);
+console.log(multi);
+console.log(div);
