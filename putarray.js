1b75b8be066043d95e266aa46689a689a3c9a45f
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-11 10:26:30 +0900

    .pust()로 배열에 값 넣기

---
+++ putarray.js
@@ -0,0 +1,10 @@
+const arnumbers = [];
+arnumbers.push(1);
+arnumbers.push(2);
+arnumbers.push(3);
+
+const arTexts = [];
+arTexts.push('hello', 'welcome', 'bye');
+
+console.log(arnumbers);
+console.log(arTexts);
