f65919fa77a74876130d5999fe31fd4f6848cdef
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-12 23:25:05 +0900

    매개변수 인자 주석 설명

--- paraargu.js
+++ paraargu.js
@@ -1,7 +1,7 @@
-function printMessage(pMessage){
+function printMessage(pMessage){//pMessage는 매개변수
     console.log(pM);
 }
-printMessage('hello');
+printMessage('hello');//hello가 인자
 
 const printMessage2 =(pMessage) =>{
     console.log(pMessage);
