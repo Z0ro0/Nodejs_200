b1abf67b568c2abce87b8b7687e27061522ef927
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-13 22:34:36 +0900

    제곱근sqrt 세제곱근cbrt

---
+++ sqrtcbrt.js
@@ -0,0 +1,19 @@
+const sqrt1 = Math.sqrt(9);//3
+const sqrt2 = Math.sqrt(1);//1
+const sqrt3 = Math.sqrt(0);//0
+const sqrt4 = Math.sqrt(-1);//Nan
+
+const cbrt1 = Math.cbrt(-8);//-2
+const cbrt2 = Math.cbrt(0);//0
+const cbrt3 = Math.cbrt(1);//1
+const cbrt4 = Math.cbrt(Infinity);//Infinity
+
+console.log(sqrt1);
+console.log(sqrt2);
+console.log(sqrt3);
+console.log(sqrt4);
+
+console.log(cbrt1);
+console.log(cbrt2);
+console.log(cbrt3);
+console.log(cbrt4);
