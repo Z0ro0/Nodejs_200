c8ce427ca06549509fb1734ad2084d0bdb9d299f
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-26 22:00:15 +0900

    오름차순 정렬 sort

---
+++ sort.js
@@ -0,0 +1,17 @@
+const numbers = [3, 6, 2, 8, 1];
+const strings = [
+    'timeoutsRemaining',
+    'flagrantFouls',
+    'defensive3Seconds',
+    'jumpshots',
+    'dunks',
+    'layups',
+];
+
+const sortedNumbers = numbers.sort();//정렬
+const sortedStrings = strings.sort();//정렬
+
+console.log(sortedNumbers);//정렬된 sortesNumbers
+console.log(sortedNumbers.reverse());//뒤집어서
+console.log(sortedStrings);//정렬된 sortedStrings
+console.log(sortedStrings.reverse());//뒤집어서
