1a9ce295f8e904e47844588288e03e2126144add
Z0ro0 <chlrkdmf0805@gmail.com>
2023-03-26 21:52:08 +0900

    배열 뒤집기

---
+++ reverse.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3,4];
+numbers.reverse();
+
+console.log(numbers);
+
+const stringArray = 'hello'.split('');//hello를 각 문자별로 나누어서 배열에 집어넣음 ['h','e','l','l','o']
+stringArray.reverse();
+console.log(stringArray);
