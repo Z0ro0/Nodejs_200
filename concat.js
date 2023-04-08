c9767eaf2618afa40539f73e2ef3185730b21beb
Z0ro0 <chlrkdmf0805@gmail.com>
2023-04-08 22:40:18 +0900

    concat() 배열 합치기

---
+++ concat.js
@@ -0,0 +1,9 @@
+const part1 = ['gaeul','doyeon'];
+const part2 = ['jaeyeon','hajin'];
+const part3 = ['chaemin','wonseo'];
+
+const team = part1.concat(part2);
+const team2 = part2.concat(part3);
+
+console.log(team);
+console.log(team2);
